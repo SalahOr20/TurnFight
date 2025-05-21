e67454742b6523fa081cc638ac3855a4e91357e1
Salahor <salahor20@gmail.com>
2025-05-21 17:17:20 +0200

    Add test for hello world

--- src/test/java/com/epsi/turnfight/TurnFightApplicationTests.java
+++ src/test/java/com/epsi/turnfight/TurnFightApplicationTests.java
@@ -3,11 +3,30 @@ package com.epsi.turnfight;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class TurnFightApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+    void testHelloTurnFightMessage() throws Exception {
+        // Sauvegarde de la sortie standard actuelle
+        PrintStream originalOut = System.out;
 
+        // Création d'un flux temporaire pour capturer la sortie
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        // Lancement de l'application
+        TurnFightApplication.main(new String[]{});
+
+        // Restauration de la sortie standard
+        System.setOut(originalOut);
+
+        // Vérifie que la sortie contient bien le message attendu
+        assertTrue(outContent.toString().contains("Hello, TurnFight!"));
+    }
 }
