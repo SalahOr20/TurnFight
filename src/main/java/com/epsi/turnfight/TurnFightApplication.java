b781fba065351ee0cacf68f71ac15012871d4b9e
AbdellaAinak <ainakabdellah@gmail.com>
2025-05-21 15:39:26 +0200

    initial commit

---
+++ src/main/java/com/epsi/turnfight/TurnFightApplication.java
@@ -0,0 +1,13 @@
+package com.epsi.turnfight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnFightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurnFightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epsi/turnfight/TurnFightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epsi.turnfight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnFightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
