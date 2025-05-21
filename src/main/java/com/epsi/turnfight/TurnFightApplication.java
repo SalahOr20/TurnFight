ed42d1185dbb1da88f0f0013ef0a49cb30ac5f35
Salahor <salahor20@gmail.com>
2025-05-21 17:12:11 +0200

    Add Hello World

--- src/main/java/com/epsi/turnfight/TurnFightApplication.java
+++ src/main/java/com/epsi/turnfight/TurnFightApplication.java
@@ -1,13 +1,18 @@
 package com.epsi.turnfight;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TurnFightApplication {
+public class TurnFightApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(TurnFightApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello, TurnFight!");
+    }
 }
